19c1ae799f5e14c646b785a45ce4e7d58ebbd726
mcpade <mariceli.paredes@gmail.com>
2019-04-27 21:21:57 +0200

    primera funcionalidad suma todos

---
+++ p1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def SumaTodos (limitTo):
+    resultado = 0
+    for i in range (0,limitTo+1):
+        resultado += i
+    
+    return resultado
+
+
+# In[8]:
+
+
+print (SumaTodos(100))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
